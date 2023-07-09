158bf0ccc7ec55160e7a0b4957158d728b3d6436
TCSiX <139041925+TCSlX@users.noreply.github.com>
2023-07-09 22:56:33 +0800

    玄幻问题更改

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import datetime
 import time 
 from selenium.webdriver.common.by import By 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
@@ -11,8 +13,8 @@ WebDriver.get("https://show.bilibili.com/platform/detail.html?id=71212&from=pc_t
 time.sleep(1)
 print("进入购票页面成功")
 WebDriver.find_element(By.CLASS_NAME, "nav-header-register").click()
-print("请在25s内登录")
-time.sleep(25)
+print("请在10s内登录")
+time.sleep(10)
 
 # if(WebDriver.find_element(By.CLASS_NAME, "product-buy")):
 #     print("确认此页面为预购页面")
@@ -36,25 +38,26 @@ while True:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     print(now + "     " + TargetTime)
     if now >= TargetTime:
-        WebDriver.refresh()
         break
 
 while True:
     try:
-        product_buy_buttons = WebDriver.find_elements(By.CLASS_NAME, "product-buy.enable")
+        wait = WebDriverWait(WebDriver, 10)
+        product_buy_buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "product-buy.enable")))
         if len(product_buy_buttons) > 0:
             print("找到购买按钮")
             product_buy_buttons[0].click()  # 点击第一个购买按钮
-            # time.sleep(5)
             print("进入购买页面成功")
             break
         else:
             print("无购买按钮")
     except:
         print("无法点击购买")
+        time.sleep(1)
 
 try:
-    confirm_paybtn = WebDriver.find_element(By.CLASS_NAME, "confirm-paybtn.active")
+    wait = WebDriverWait(WebDriver, 10)
+    confirm_paybtn = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "confirm-paybtn.active")))
     confirm_paybtn.click()
     print("订单创建完成，请在一分钟内付款")
     # time.sleep(60)
